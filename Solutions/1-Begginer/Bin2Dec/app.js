44c6d8b39269b0f3bbba344ce3fd0ec9be7b8bda
Thiago Assis <thiagosil@gmail.com>
2021-01-08 15:34:05 -0300

    Adding basic bin2Dec solution and specs

--- Solutions/1-Begginer/Bin2Dec/app.js
+++ Solutions/1-Begginer/Bin2Dec/app.js
@@ -1,3 +1,19 @@
 export function bin2Dec(number){
-    return 0;
+  if (number.length > 8){
+    return 'Invalid Length';
   }
+
+  if(number.match(/^[0-1]+$/) === null){
+    return 'Only 0 or 1 is accepted';
+  }
+
+  let numbers = Array.from(number).reverse();
+
+  let total = 0;
+  for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] === '1'){
+      total += Math.pow(2, i);
+    }
+  }
+  return total;
+}
--- Solutions/1-Begginer/Bin2Dec/app.test.js
+++ Solutions/1-Begginer/Bin2Dec/app.test.js
@@ -1,8 +1,33 @@
 import { bin2Dec } from './app';
 
 describe('App', () => {
-  test('the first verse', () => {
+  test('zero', () => {
     const number = '00000000';
     expect(bin2Dec(number)).toBe(0);
+  });
+
+  test('1', () => {
+    const number = '00000001';
+    expect(bin2Dec(number)).toBe(1);
+  });
+
+  test('2', () => {
+    const number = '00000010';
+    expect(bin2Dec(number)).toBe(2);
+  });
+
+  test('3', () => {
+    const number = '0000011';
+    expect(bin2Dec(number)).toBe(3);
+  });
+
+  test('Invalid Length', () => {
+    const number = '000001111';
+    expect(bin2Dec(number)).toBe('Invalid Length');
+  });
+
+  test('Invalid Char', () => {
+    const number = '00003111';
+    expect(bin2Dec(number)).toBe('Only 0 or 1 is accepted');
   });
 });
